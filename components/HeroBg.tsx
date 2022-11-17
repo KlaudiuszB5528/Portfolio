322c6a1bfd178b5b44f4bb6421332d160b00550a
KlaudiuszB5528 <klaudiusz.biegacz@gmail.com>
2022-11-17 16:29:03 +0100

    bg opacity increase

--- components/HeroBg.tsx
+++ components/HeroBg.tsx
@@ -8,21 +8,21 @@ const HeroBg = () => {
         </q>
         <p> – Bijay Chhetri</p>
       </div>
-      <div className="absolute right-20 top-24 lg:right-11 lg:top-40 opacity-5 w-96 text-xs sm:text-sm">
+      <div className="absolute right-20 top-24 lg:right-11 lg:top-40 opacity-10 w-96 text-xs sm:text-sm">
         <q>
           To program is to translate between the chaos of the chaos of the human
           life and the line-by-line world of the computer.
         </q>
         <p> - Ellen Ullman</p>
       </div>
-      <div className="absolute right-4 top-40 lg:right-40 lg:top-[45%] opacity-5 text-xs sm:text-sm w-40">
+      <div className="absolute right-4 top-40 lg:right-40 lg:top-[45%] opacity-10 text-xs sm:text-sm w-40">
         <q>
           {` “There are three responses to a piece of design - yes, no, and WOW! Wow
         is the one to aim for”`}
         </q>
         <p> - Milton Glaser</p>
       </div>
-      <div className="absolute hidden lg:block left-72 bottom-36 opacity-5 text-xs sm:text-sm w-44">
+      <div className="absolute hidden lg:block left-72 bottom-36 opacity-10 text-xs sm:text-sm w-44">
         <q>
           Learning to write programs stretches your mind, and helps you think
           better, creates a way of thinking about things that I think is helpful
@@ -30,7 +30,7 @@ const HeroBg = () => {
         </q>
         <p> - Bill Gates</p>
       </div>
-      <div className="absolute right-2 bottom-8 md:right-2 xl:right-20 md:bottom-32 opacity-5 text-xs sm:text-sm">
+      <div className="absolute right-2 bottom-8 md:right-2 xl:right-20 md:bottom-32 opacity-10 text-xs sm:text-sm">
         <q>
           {
             " If you're already a front-end developer, well, pretend you're also wearing a pirate hat."
